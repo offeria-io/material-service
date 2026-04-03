a084efbf0a9caf00e8fc8a0235a24676b3b8ed36
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-04-03 16:23:53 +0300

    Add Maven wrapper and initial Spring Boot project setup.

---
+++ src/main/java/offeria/material_service/MaterialServiceApplication.java
@@ -0,0 +1,13 @@
+package offeria.material_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/offeria/material_service/MaterialServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package offeria.material_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
